df46819d5e1969b8346dc40b70e00b6aa09a996c
Jacek <trojanowski.jacek@gmail.com>
2020-08-31 09:17:39 +0200

    add function random and list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 import '../sass/style.scss';
+
+const listBreeds = () => {
+    return fetch('https://dog.ceo/api/breeds/list/all')
+        .then(res => res.json())
+        .then(data => {
+            return data.message
+        })
+}
+
+const getRandomImage = () => {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            return data.message
+        })
+}
+
+getRandomImage().then(data => console.log(data))
